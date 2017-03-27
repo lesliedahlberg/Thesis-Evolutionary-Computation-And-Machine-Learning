bcd8e83de1ffc6e7fd0af6a0c827438e6d84c6df
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2017-03-27 22:09:51 +0200

    CLU & FIT

--- Code/benchmarks/BenchmarkANN_CLASS.m
+++ Code/benchmarks/BenchmarkANN_CLASS.m
@@ -1,7 +1,7 @@
 clc
-ALG = {@DE @PSO @EDA_UMDA };
-algorithms = 3;
-algNames = { 'DE' 'PSO' 'EDA_UMDA'};
+ALG = {@DE @PSO @EDA_UMDA @DEDA};
+algorithms = 4;
+algNames = {'DE' 'PSO' 'UMDA' 'DEDA'};
 
 [xxxx{1},tt{1}] = simpleclass_dataset;
 [xxxx{2},tt{2}] = cancer_dataset;
@@ -14,14 +14,17 @@ setNames = {'simple' 'cancer' 'crab' 'glass' 'iris' 'thyroid' 'wine' };
 
 datasets = 7;
 
+
 objectiveValue = 0;
 individuals = 50;
-generations = 50;
+generations = 250;
 lower = -1;
 upper = 1;
+repeat = 5;
 
 v = ones(datasets,algorithms);
-
+s = ones(datasets,algorithms);
+tic
 for ds=1:datasets
     xx = xxxx{ds};
     t = tt{ds};
@@ -31,16 +34,33 @@ for ds=1:datasets
     outputSize = size(t,1);
 
     layers = [inputSize,hiddenSize,outputSize];
+    ds
     dim = NeuralNetworkSize(layers)
 
-
+    g = generations;
 
     for alg=1:algorithms
-        A = alg
-        eval = @(x)ann_dataset_sse(x, layers, xx, t);
-        [success, iterations, minimum, value] = ALG{alg}(eval, dim, lower, upper, generations, individuals, objectiveValue);
-        v(ds,alg) = value;
+        mins = zeros(1, repeat);
+        fprintf(strcat('\ta=',num2str(alg)));
+        
+        for r=1:repeat
+            
+            eval = @(x)ann_dataset_sse(x, layers, xx, t);
+            [success, iterations, minimum, value] = ALG{alg}(eval, dim, lower, upper, g, individuals, objectiveValue);
+            
+            mins(r) = value;
+            fprintf('*');
+        end
+        v(ds,alg) = mean(mins);
+        s(ds,alg) = std(mins);
+        fprintf('\n');
+        
+        
     end
 end
+time = toc
+tm = table(v(:,1), v(:,2), v(:,3), v(:,4), 'RowNames', setNames, 'VariableNames', algNames)
+td = table(s(:,1), s(:,2), s(:,3), s(:,4), 'RowNames', setNames, 'VariableNames', algNames)
 
-table(v(:,1), v(:,2), v(:,3), 'RowNames', setNames, 'VariableNames', algNames)
+writetable(tm,strcat('ann_cls_mean.xlsx'));
+writetable(td,strcat('ann_cls_dev.xlsx'));
